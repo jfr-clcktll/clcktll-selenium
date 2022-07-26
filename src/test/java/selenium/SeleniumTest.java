c0f27984b0b0c49bcfef03821ed96e63c73c1df2
Ali Jaffar <ali.jaffar@clickatell.com>
2022-07-25 20:52:51 -0400

    Session 7 exercise 2 Login to page

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -3,24 +3,35 @@ package selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
-
 
 public class SeleniumTest {
 
-    String websiteLink = "http://parabank.parasoft.com";
+    String WEBLINK_PARABANK = "http://parabank.parasoft.com";
+    String USERNAME = "john";
+    String PASSWORD = "demo";
+    int DEFAULT_WAIT_TIME = 3000;
+
 
     @Test
-    public void startAndStopABrowser() throws InterruptedException {
+    public void verifyParaBankLogin() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
-        driver.get(websiteLink);
+        driver.get(WEBLINK_PARABANK);
+        Thread.sleep(DEFAULT_WAIT_TIME);
 
-        Thread.sleep(3000);
+        driver.findElement(By.name("username")).sendKeys(USERNAME);
+        Thread.sleep(1000);
+
+        driver.findElement(By.name("password")).sendKeys(PASSWORD);
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+        Thread.sleep(DEFAULT_WAIT_TIME);
 
         driver.quit();
     }
