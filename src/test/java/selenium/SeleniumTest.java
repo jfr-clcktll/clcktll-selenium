94dd7b15f05a98c845bbfb6a4ec8f836f63cfd6d
Ali Jaffar <ali.jaffar@clickatell.com>
2022-07-25 23:36:59 -0400

    Session 7 exercise 4 BeforeAfterMethods

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -6,31 +6,39 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import javax.xml.ws.WebEndpoint;
 import java.util.List;
+import java.util.Locale;
 
 public class SeleniumTest {
 
     // PARAMETERS
-    String WEBLINK_PARABANK = "http://parabank.parasoft.com";
+    String WEBLINK = "http://parabank.parasoft.com";
     String USERNAME = "john";
     String PASSWORD = "demo";
     int DEFAULT_WAIT_TIME = 3000;
 
     String PAGE_TITLE = "ParaBank | Accounts Overview";
 
+    WebDriver driver; // This needs to be defined outside the methods so variables / classes can transfer from @Before to @Test scopes
 
-    @Test
-    public void verifyParaBankLogin() throws InterruptedException {
-
+    @BeforeMethod
+    public void setupBrowser() throws InterruptedException{
         WebDriverManager.chromedriver().setup(); // Setup browser and driver versions
-        WebDriver driver = new ChromeDriver(); // Initialize Chrome Driver
-
+        driver = new ChromeDriver(); // Initialize Chrome Driver
         driver.manage().window().maximize(); // Maximize the browser window
-        driver.get(WEBLINK_PARABANK); // Opens this address in Chrome
         Thread.sleep(DEFAULT_WAIT_TIME);
 
+    }
+
+
+    @BeforeMethod
+    public void verifyParaBankLogin() throws InterruptedException {
+
+        driver.get(WEBLINK); // Opens this address in Chrome
+        Thread.sleep(DEFAULT_WAIT_TIME);
 
         driver.findElement(By.name("username")).sendKeys(USERNAME); // Find username field and enters username
         Thread.sleep(1000);
@@ -41,17 +49,36 @@ public class SeleniumTest {
         driver.findElement(By.xpath("//input[@value='Log In']")).click(); // Find Log in button field and click it
         Thread.sleep(DEFAULT_WAIT_TIME);
 
+    }
 
+    @Test
+    public void verifyHeadingsLandingPage() throws InterruptedException{
         String title = driver.getTitle(); // Get page title
         Assert.assertEquals(title, PAGE_TITLE); // Verify page title
-
-        //WebElement tabHeading = driver.findElement(By.xpath("/html/head/title")); // Find an element with said ID
-        //Assert.assertTrue(tabHeading.equals("ParaBank | Accounts Overview"));
-
 
         WebElement accountsOverviewTitle = driver.findElement(By.xpath("//h1[text()='Accounts Overview']")); // Find an element with said xpath
         Assert.assertTrue(accountsOverviewTitle.isDisplayed()); // Verify that element is displayed on page
 
+    }
+
+    @Test
+    public void verifyLoanApplication() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id='leftPanel']/ul/li[7]/a")).click(); // Find and click Request Loan Link
+        Thread.sleep(DEFAULT_WAIT_TIME);
+        driver.findElement(By.id("amount")).sendKeys("1000"); // Enter 1000 in the Loan Amount field
+        driver.findElement(By.id("downPayment")).sendKeys("100"); // Enter 100 in the Down Payment field
+        driver.findElement(By.xpath("//input[@value='Apply Now']")).click(); // Find Apply  in button field and click it
+        Thread.sleep(DEFAULT_WAIT_TIME);
+
+        String loanStatus = driver.findElement(By.id("loanStatus")).getText();
+        System.out.println("xxxxxxxxxxx-------------------xxxxxxxxxxx");
+        System.out.println("Your loan has been " + loanStatus.toUpperCase());
+        System.out.println("xxxxxxxxxxx-x------------------xxxxxxxxxxx");
+
+    }
+
+    @AfterMethod
+    public void quitBrowser() {
         driver.quit(); // Exit the browser
     }
 }
